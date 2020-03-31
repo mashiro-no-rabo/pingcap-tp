7cc1091bcc56c3e8b293ece7f6af11bb2d8c8bc5
LOU Xun <aquarhead@ela.build>
2020-03-31 21:38:44 +0000

    PNA Rust 2, project setup

--- kvs/src/lib.rs
+++ kvs/src/lib.rs
@@ -3,6 +3,17 @@
 //! A library for in-memory key-value store
 
 use std::collections::HashMap;
+use std::path::Path;
+use thiserror::Error;
+
+/// Error kinds enum for KvStore operations
+#[derive(Debug, Error)]
+pub enum KvStoreError {
+  //
+}
+
+/// Result wrapper for KvStore operations
+pub type Result<T> = std::result::Result<T, KvStoreError>;
 
 /// KvStore is an in-memory key-value store
 pub struct KvStore {
@@ -11,22 +22,24 @@ pub struct KvStore {
 
 impl KvStore {
   /// Creates a new key-value store
-  pub fn new() -> Self {
-    Self { store: HashMap::new() }
+  pub fn open<P: AsRef<Path>>(_: P) -> Result<Self> {
+    Ok(Self { store: HashMap::new() })
   }
 
   /// Get the value associated with the given key in the key-value store
-  pub fn get(&self, key: String) -> Option<String> {
-    self.store.get(&key).map(|s| s.to_owned())
+  pub fn get(&self, key: String) -> Result<Option<String>> {
+    Ok(self.store.get(&key).map(|s| s.to_owned()))
   }
 
   /// Set the value associated with the given key in the key-value store
-  pub fn set(&mut self, key: String, value: String) {
+  pub fn set(&mut self, key: String, value: String) -> Result<()> {
     self.store.insert(key, value);
+    Ok(())
   }
 
   /// Remove the given key and its associated value from the key-value store
-  pub fn remove(&mut self, key: String) {
+  pub fn remove(&mut self, key: String) -> Result<()> {
     self.store.remove(&key);
+    Ok(())
   }
 }
