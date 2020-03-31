64842ca9436b8918d93378ebc3ea56eefd31f978
LOU Xun <aquarhead@ela.build>
2020-03-31 21:20:36 +0000

    Add other exercises in PNA Rust bb2

--- rustbb2/src/main.rs
+++ rustbb2/src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
+use std::io::Cursor;
 
 #[derive(Debug, Serialize, Deserialize)]
 enum Direction {
@@ -31,6 +32,38 @@ fn main() -> Result<()> {
   let b: Move = serde_json::from_reader(f)?;
 
   println!("[exercise1] b: {:?}", &b);
+
+  let v8 = serde_json::to_vec(&a)?;
+  let b_v8: Move = serde_json::from_slice(&v8)?;
+
+  println!("[exercise2] b from Vec<u8>: {:?}", &b_v8);
+
+  let s = ron::ser::to_string(&a)?;
+  println!("[exercise2] ron: {:?}", &s);
+  let b_ron: Move = ron::de::from_str(&s)?;
+
+  println!("[exercise2] b from Ron: {:?}", &b_ron);
+
+  let mut moves: bson::Array = vec![];
+  for d in 1..=1000 {
+    let m = Move {
+      dir: Direction::Up,
+      dist: d,
+    };
+
+    moves.push(bson::to_bson(&m)?);
+  }
+
+  let mut doc = bson::Document::new();
+  doc.insert("moves".to_owned(), moves);
+
+  let mut buf = Vec::new();
+  bson::encode_document(&mut buf, &doc)?;
+
+  let doc = bson::decode_document(&mut Cursor::new(&buf[..]))?;
+  for m in doc.get_array("moves")?.iter().take(3) {
+    println!("[exercise3] move: {:?}", m);
+  }
 
   Ok(())
 }
