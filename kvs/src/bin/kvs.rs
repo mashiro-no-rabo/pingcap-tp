07075a07cadc5f6ef9c31406a8632aa22a35266b
LOU Xun <aquarhead@ela.build>
2020-04-01 21:21:42 +0000

    Pass all cli test in PNA Rust proj 2

--- kvs/src/bin/kvs.rs
+++ kvs/src/bin/kvs.rs
@@ -1,5 +1,7 @@
-use anyhow::{bail, Result};
+use anyhow::Result;
 use structopt::StructOpt;
+
+use kvs::*;
 
 #[derive(Debug, StructOpt)]
 #[structopt(
@@ -14,8 +16,32 @@ enum Kv {
 
 fn main() -> Result<()> {
   match Kv::from_args() {
-    Kv::Get { key: _ } => bail!("unimplemented"),
-    Kv::Set { key: _, value: _ } => bail!("unimplemented"),
-    Kv::Rm { key: _ } => bail!("unimplemented"),
+    Kv::Get { key } => {
+      let store = KvStore::open(".")?;
+      match store.get(key)? {
+        Some(vv) => println!("{}", vv),
+        None => println!("Key not found"),
+      }
+      Ok(())
+    }
+    Kv::Set { key, value } => {
+      let mut store = KvStore::open(".")?;
+      store.set(key, value)?;
+      Ok(())
+    }
+    Kv::Rm { key } => {
+      let mut store = KvStore::open(".")?;
+      let handled_not_found = match store.remove(key) {
+        Err(KvStoreError::RmKeyNotFoundError) => {
+          println!("Key not found");
+          std::process::exit(1);
+        }
+        other => other,
+      };
+
+      handled_not_found?;
+
+      Ok(())
+    }
   }
 }
--- kvs/src/lib.rs
+++ kvs/src/lib.rs
@@ -2,8 +2,11 @@
 
 //! A library for in-memory key-value store
 
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::path::Path;
+use std::fs::{File, OpenOptions};
+use std::io::{prelude::*, BufReader};
+use std::path::{Path, PathBuf};
 use thiserror::Error;
 
 /// Error kinds enum for KvStore operations
@@ -12,36 +15,113 @@ use thiserror::Error;
 pub enum KvStoreError {
   #[error("I/O operation failed")]
   IoError(#[from] std::io::Error),
+  #[error("Replay existing log file failed: {0}")]
+  ReplayError(String),
+  #[error("Serializing command failed")]
+  SerError(#[from] ron::ser::Error),
+  #[error("Deserializing command failed")]
+  DeError(#[from] ron::de::Error),
+  #[error("Key not found during remove")]
+  RmKeyNotFoundError,
 }
 
 /// Result wrapper for KvStore operations
 pub type Result<T> = std::result::Result<T, KvStoreError>;
 
+// the in-memory index type
+type Index = HashMap<String, String>;
+// the log type
+type Log = File;
+
 /// KvStore is an in-memory key-value store
 pub struct KvStore {
-  store: HashMap<String, String>,
+  index: Index,
+  log: Log,
+}
+
+type Key = String;
+type Value = String;
+#[derive(Debug, Serialize, Deserialize)]
+enum KvCommand {
+  Set(Key, Value),
+  Rm(Key),
 }
 
 impl KvStore {
   /// Creates a new key-value store
-  pub fn open<P: AsRef<Path>>(_: P) -> Result<Self> {
-    Ok(Self { store: HashMap::new() })
+  pub fn open(path: impl Into<PathBuf>) -> Result<Self> {
+    let log_path = path.into().join("kvs.log");
+    let index = Self::replay(&log_path)?;
+    let log = OpenOptions::new()
+      .append(true)
+      .read(true)
+      .create(true)
+      .open(&log_path)?;
+
+    Ok(Self { index, log })
   }
 
   /// Get the value associated with the given key in the key-value store
   pub fn get(&self, key: String) -> Result<Option<String>> {
-    Ok(self.store.get(&key).map(|s| s.to_owned()))
+    Ok(self.index.get(&key).map(|s| s.to_owned()))
   }
 
   /// Set the value associated with the given key in the key-value store
   pub fn set(&mut self, key: String, value: String) -> Result<()> {
-    self.store.insert(key, value);
+    // write log
+    let cmd = KvCommand::Set(key.clone(), value.clone());
+    write!(self.log, "{}\n", ron::ser::to_string(&cmd)?)?;
+
+    // update in-memory index
+    self.index.insert(key, value);
+
     Ok(())
   }
 
   /// Remove the given key and its associated value from the key-value store
   pub fn remove(&mut self, key: String) -> Result<()> {
-    self.store.remove(&key);
+    // check exist
+    if !self.index.contains_key(&key) {
+      return Err(KvStoreError::RmKeyNotFoundError);
+    }
+
+    // write log
+    let cmd = KvCommand::Rm(key.clone());
+    write!(self.log, "{}\n", ron::ser::to_string(&cmd)?)?;
+
+    // update in-memory index
+    self.index.remove(&key);
+
     Ok(())
+  }
+
+  fn replay<P: AsRef<Path>>(path: P) -> Result<Index> {
+    let mut index = Index::new();
+
+    match File::open(path) {
+      Ok(f) => {
+        let reader = BufReader::new(f);
+        for cmd_string in reader.lines().map(|l| l.unwrap()) {
+          let cmd = ron::de::from_str(&cmd_string)?;
+          match cmd {
+            KvCommand::Set(key, value) => {
+              index.insert(key, value);
+            }
+            KvCommand::Rm(key) => {
+              index.remove(&key);
+            }
+          }
+        }
+
+        Ok(index)
+      }
+      Err(e) => {
+        if e.kind() == std::io::ErrorKind::NotFound {
+          Ok(index)
+        } else {
+          Err(KvStoreError::IoError(e))
+        }
+      }
+    }
   }
 }
